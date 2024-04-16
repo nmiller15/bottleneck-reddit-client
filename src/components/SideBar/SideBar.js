859c0588bf81c24c83bcaa1e20a80849af1bd22a
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-04-16 07:04:23 -0400

    style logo in sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './SideBar.css';
+import logo from '../../resources/bottleneck-logo.svg'
   
   const SideBar = () =>  {
 	return (
 	  <div className="SideBar">
         <div id="headline-container">
             <div id="logo-container">
-                <img src="../../resources/glass-bottle-icon.svg" alt="Wine bottle icon"/>
+                <img src={logo} alt="Wine bottle icon"/>
             </div>
             <div id="text-container">
                 <h1>Bottleneck</h1>
