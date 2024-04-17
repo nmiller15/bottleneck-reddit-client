5f60f2f18791f5f4d5be7ee5ae459f38601b4dc5
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-04-17 07:30:27 -0400

    add header to feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,28 @@
 import React from 'react';
+// import SearchBar from '../SearchBar/SearchBar.js'
 import './Feed.css';
+import searchIcon from '../../resources/Search Icon.svg'
   
   const Feed = () =>  {
 	return (
 	  <div className="Feed">
-        <p>This is the Feed component.</p>
+        <header>
+            <div id="filters">
+                <ul>
+                    <li>Best</li>
+                    <li>Hot</li>
+                    <li>New</li>
+                    <li>Top</li>
+                    <li>Rising</li>
+                </ul>
+            </div>
+            <div id="search-bar-container">
+            
+            </div>
+            <div id="search-icon-container">
+                <img src={searchIcon} alt="search"/>
+            </div>
+        </header>
 	  </div>
 	);
   }
