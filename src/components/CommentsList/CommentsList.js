0b8b316dcca99910321b4cec017c08cb49670325
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-04-19 11:53:44 -0400

    fix array map

--- src/components/CommentsList/CommentsList.js
+++ src/components/CommentsList/CommentsList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Comment from '../Comment/Comment.js';
   
-  const CommentsList = ({comments}) =>  {
-
-	
-        comments.map((comment) => {
-            return <Comment comment={comment} />
-        })
-
+  const CommentsList = ({ comments }) =>  {
+    console.log(comments);
+	return (
+        <>
+            {comments.map((comment, index) => {
+                <Comment comment={comment} />
+            })}
+        </>
+        
+    )
   }
   
   export default CommentsList;
