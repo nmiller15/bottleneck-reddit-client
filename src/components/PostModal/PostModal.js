444d5425c09226efbb7ec989a1ea15ee33fbd522
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-04-23 22:23:23 -0400

    implement fetch API in PostModal

--- src/components/PostModal/PostModal.js
+++ src/components/PostModal/PostModal.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CommentsList from '../CommentsList/CommentsList.js';
 import Voter from '../Voter/Voter.js';
+import Media from '../Media/Media.js';
 import mockJson from '../../mock/reddit-post-mock.json';
 import voterMock from '../../resources/voter_mock.svg';
 import exit from '../../resources/X.svg';
 import './PostModal.css';
 
+// Once the store is set up, the comment button must set the permalink and pass it down to this component
+// Sample image perma: "permalink": "/r/pics/comments/1c6e2t7/sarah_huckabee_sanders_paid_19_000_for_this/",
+// Sample video perma: "permalink": "/r/Damnthatsinteresting/comments/1c6hah3/ojs_reaction_when_confronted_with_a_photo_of_him/",
+// Sample link perma: "permalink": "/r/nba/comments/1c6dz5u/charania_raptors_jontay_porter_has_received_a/",
 
-const PostModal = () =>  {
-    console.log(mockJson);
-    const postArray = mockJson[0].data.children;
+const PostModal = ( /*{ permalink }*/ ) =>  {
+    // Mocked permalink prop
+    const permalink = "/r/Damnthatsinteresting/comments/1c6hah3/ojs_reaction_when_confronted_with_a_photo_of_him/"
+    const [postData, setPostData] = useState(null);
 
-    const postHint = postArray[0].data.post_hint;
+    useEffect(() => {
+        const fetchPostData = async () => {
+            try {
+                const response = await fetch(`https://api.reddit.com${permalink}.json`);
+                if (!response.ok) {
+                    throw new Error('Network response failed.');
+                }
+                //console.log(response);
+                const postArray = await response.json();
+                //console.log(postArray);
+                // Handle any errors, and filter out galleries
+                if (!postArray || postArray[0].data.children[0].data.is_gallery === true) {
+                    return;
+                }
+                const post = postArray[0].data.children[0];
+                
+                setPostData({
+                    title: post.data.title,
+                    description: post.data.selftext,
+                    author: post.data.author,
+                    subreddit: post.data.subreddit_name_prefixed,
+                    score: post.data.score,
+                    numComments: post.data.num_comments,
+                    commentArray: mockJson[1].data.children,
+                    post: post
+                })
+                
+                
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-    const title = postArray[0].data.title;
-    const description = postArray[0].data.selftext;
-    const author = postArray[0].data.author;
-    const subreddit = postArray[0].data.subreddit_name_prefixed;
-    const score = postArray[0].data.score;
-    //const url = postArray[0].data.url;
-    const url = postArray[0].data.media.reddit_video.fallback_url;
-    const numComments = postArray[0].data.num_comments;
-    const permalink = postArray[0].data.permalink;
-
-    const commentArray = mockJson[1].data.children; 
-
-    console.log(permalink);
+        fetchPostData();
+    }, [permalink]); // Run this effect with permalink changes
+    
+    if (!postData) {
+        return <div>Loading...</div>;
+    }
 
 	return (
-	    <div className="PostModal">
+	       <div className="PostModal">
             <div id="exit-button">
                 <img src={exit} alt="" />
             </div>
@@ -36,37 +66,37 @@ const PostModal = () =>  {
                 <div className="post-header">
                     <div className="title-container">
                         <div className="voter">
-                            <Voter score={score} />
+                            <Voter score={postData.score} />
                         </div>
                         <div className="title">
                             <p>
-                                {title}
+                                {postData.title}
                             </p>
                         </div>
                     </div>
                     <div className="description">
                         <p>
-                            {description}
+                            {postData.description}
                         </p>
                     </div>
                 </div>
-                <div className="video-container">
-                    <video src={url} controls></video>
+                <div className="media">
+                    <Media post={postData.post} title={postData.title}/>
                 </div>
                 <div className="post-footer">
                     <div className="child subreddit-of-post">
-                        <p>{subreddit}</p>
+                        <p>{postData.subreddit}</p>
                     </div>
                     <div className="child author-of-post">
-                        <p>u/{author}</p>
+                        <p>u/{postData.author}</p>
                     </div>
                 </div>
             </div>
             <div className="comments-container">
-                <h3>Comments <span class="accent">({numComments})</span></h3>
-                <CommentsList comments={commentArray} />
+                <h3>Comments <span class="accent">({postData.numComments})</span></h3>
+                <CommentsList comments={postData.commentArray} />
             </div>
-        </div>
+      </div>
 	);
   }
   
