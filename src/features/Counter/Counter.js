cbe06b6382cbf015f6ab3e19df3c6691688a7e7e
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-04-29 17:51:41 -0400

    add count state to the counter div

--- src/features/Counter/Counter.js
+++ src/features/Counter/Counter.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Counter.css';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementCount } from './counterSlice';
+
 const Counter = () => {
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(incrementCount());
+    }
     return (
         <div id="Counter">
             <div id="clock-container">
                 <p>0:15</p>
             </div>
-            <div id="call-count-container">
-                <p>6</p>
+            <div id="call-count-container" onClick={handleClick}>
+                <p>{10 - count}</p>
             </div>
             <div id="label-container">
                 <p>requests until your<br/>rate is limited</p>
--- src/features/Counter/counterSlice.js
+++ src/features/Counter/counterSlice.js
@@ -1,14 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+    count: 0
+}
+
 const options = {
     name: 'counter',
-    initialState: [], 
+    initialState,
     reducers: {
-
+        incrementCount: (state) => {
+            state.count++
+            return state;
+        }
     }
 }
 
 const counterSlice = createSlice(options);
   
-// export your actions with export const { named actions } = postModalSlice.actions;
+export const { incrementCount } = counterSlice.actions;
 export default counterSlice.reducer;
