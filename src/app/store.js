4eedfaeaa5f40a5057f34f4dbcbe5b9d3ff810fb
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-04-29 17:08:31 -0400

    manage filters state

--- src/app/store.js
+++ src/app/store.js
@@ -5,12 +5,14 @@ import subredditsReducer from '../features/Subreddits/subredditsSlice';
 import counterReducer from '../features/Counter/counterSlice';
 import timerReducer from '../features/Timer/timerSlice';
 import feedReducer from '../components/Feed/feedSlice';
+import filtersReducer from '../features/Filters/filtersSlice';
 
 const store =  configureStore({
     reducer: {
         postModal: postModalReducer,
         searchBar: searchBarReducer,
         subreddits: subredditsReducer,
+        filters: filtersReducer,
         timer: timerReducer,
         counter: counterReducer,
         feed: feedReducer,
--- src/features/Filters/Filters.js
+++ src/features/Filters/Filters.js
@@ -4,19 +4,24 @@ import './Filters.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSelectedFilter } from './filtersSlice';
 
-  const Filters = () =>  {
+const Filters = () =>  {
+  const dispatch = useDispatch();
+  const filtersArray = ["Best", "Hot", "New", "Top", "Rising"];
+  
+  const handleClick = (index) => {
+    dispatch(setSelectedFilter(filtersArray[index]));
+  }
+  
 	return (
         <div id="filters">
             <ul>
-                <li>Best</li>
-                <li>Hot</li>
-                <li>New</li>
-                <li>Top</li>
-                <li>Rising</li>
+                {filtersArray.map((filter, index) => {
+                  return <li key={index} onClick={() => handleClick(index)}>{filter}</li>
+                })}
             </ul>
         </div>
 	);
   }
   
-  export default Filters;
+export default Filters;
   
