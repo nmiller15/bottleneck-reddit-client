dff6c72a0b03d6ce59175f0f5f41cc5042a8846c
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-05-03 09:41:01 -0400

    add resposive menu button

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,37 +5,53 @@ import './SideBar.css';
 import logo from '../../resources/bottleneck-logo.svg'
   
   const SideBar = () =>  {
-	return (
-	  <div className="SideBar">
-        <div id="headline-container">
-            <div id="logo-container">
-                <img src={logo} alt="Wine bottle icon"/>
-            </div>
-            <div id="text-container">
-                <h1>Bottleneck</h1>
-            </div>
-        </div>
-        <div id="tagline">
-            <p>Browsing the front page of the internet,<br/> one minute at a time... </p>
-        </div>
-        
-        <Subreddits />
-        
-        <Counter />
+	const screenWidth = window.innerWidth;
 
-        <div id="cta">
-            <div id="cta-button">
-                <a href="mailto:contact@reddit.com">
-                    Contact Reddit
-                </a>
-            </div>
-            <div id="cta-body">
-                <p>
-                If using reddit this way is annoying, then tell Reddit about it! If Reddit is truly the front page of the internet, we shouldn’t be stuck on their home page. Write an email to contact@reddit.com.
-                </p>
-            </div>
+    return (
+	  <>
+        <div className={ screenWidth > 932 ? 
+                            "activate-sidebar-button below-screen" :
+                            "activate-sidebar-button"}
+        >
+            <div className="logo-container">
+                  <img src={logo} alt="Wine bottle icon"/>
+              </div>
         </div>
-	  </div>
+        <div 
+          className={ screenWidth > 932 ?
+                      "SideBar" :
+                      "SideBar collapsed"}
+        >
+          <div id="headline-container">
+              <div className="logo-container">
+                  <img src={logo} alt="Wine bottle icon"/>
+              </div>
+              <div id="text-container">
+                  <h1>Bottleneck</h1>
+              </div>
+          </div>
+          <div id="tagline">
+              <p>Browsing the front page of the internet,<br/> one minute at a time... </p>
+          </div>
+            
+          <Subreddits />
+            
+          <Counter />
+
+          <div id="cta">
+              <div id="cta-button">
+                  <a href="mailto:contact@reddit.com">
+                      Contact Reddit
+                  </a>
+              </div>
+              <div id="cta-body">
+                  <p>
+                  If using reddit this way is annoying, then tell Reddit about it! If Reddit is truly the front page of the internet, we shouldn’t be stuck on their home page. Write an email to contact@reddit.com.
+                  </p>
+              </div>
+          </div>
+	    </div>
+      </>
 	);
   }
   
